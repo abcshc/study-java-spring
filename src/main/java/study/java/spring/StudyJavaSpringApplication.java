a29ab11d9b0cbb86e5022db169b6d0ab31179f63
Heechul So <thrrkthrrk@gmail.com>
2021-07-11 11:08:49 +0900

    Initialize project

---
+++ src/main/java/study/java/spring/StudyJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package study.java.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJavaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyJavaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/java/spring/StudyJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package study.java.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJavaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
